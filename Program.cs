9f6870a525b1e0813efb399d724127a59ad93172
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-08-25 19:16:25 +0545

    tried to show tictactoe grid

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class UI {
+   static void Main(string [] args) {
+      
+   }
+}
+
+class Program {
+   string [] cells = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+  showGrid(); 
+   public void showGrid() {
+      for (int i = 0; i < 3; i++) {
+        Console.WriteLine(cells[i + '|']);
+      for (int j = 0; j < 3; j++) {
+           Console.WriteLine("~~~~~~");
+         }
+      }
+   }
+}
