97fa7d68cfeda43b4b49861cb0b45a8f8dd6a1ce
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-08-26 09:26:25 +0545

    tried again to print out the grid of the tictactoe game but failed

--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
-﻿using System;
+﻿using System;  
+namespace Tic-Tac-Toe
+{
 class UI {
    static void Main(string [] args) {
-      
+     Console.WriteLine("Tic-Tac-Toe game");
+     Program.showGrid(); 
    }
 }
 
 class Program {
-   string [] cells = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+   public static string [] cells = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-  showGrid(); 
-   public void showGrid() {
-      for (int i = 0; i < 3; i++) {
-        Console.WriteLine(cells[i + '|']);
-      for (int j = 0; j < 3; j++) {
-           Console.WriteLine("~~~~~~");
+      static void showGrid() {
+          for (int i = 0; i < 3; i++) {
+             for (int j = 0; j < 3; j++) {
+                Console.WriteLine();
+                Console.WriteLine("~~~~~~");
+            }
+            Console.WriteLine(cells[i] + '|');
          }
+          //end of loop and showGrid function//
       }
-   }
+}
 }
