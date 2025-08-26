df5b3e8ab3da2baf38beb9a0eb5e44c2b4646343
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-08-26 12:43:40 +0545

    Printed out the tictactoe grid, now using 2d arrays

--- Program.cs
+++ Program.cs
@@ -7,17 +7,22 @@ class UI {
 }
 
 class Program {
-   public static string [] cells = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+   public static string [,] cells =  { // string[][] can also be used
+                                       {"1", "2", "3"},
+                                       {"4", "5", "6"},
+                                       {"7", "8", "9"},
+                                    };
 
       public static void showGrid() {
-          for (int i = 0; i < 3; i++) {
-             for (int j = 0; j < 3; j++) {
-               Console.Write(cells[i]);   
+         for (int i = 0; i < cells.GetLength(0); i++) {
+            for (int j = 0; j < cells.GetLength(1); j++) {
+               Console.Write(' '+cells[i,j]);
                Console.Write("|");
             }
             Console.WriteLine();
-            Console.WriteLine("~~~~~~");
+            Console.WriteLine(" " + "~-~-~-~-");
          }
-          //end of loop and showGrid function//
       }
 }
+
+
