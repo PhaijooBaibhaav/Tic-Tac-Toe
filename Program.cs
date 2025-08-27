5a63237c8377a7c70079e8cd29752be6c7914ac7
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-08-27 19:09:24 +0545

    Added enum for gamemode X and O/

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ class UI {
      {
         Program.showGrid(); 
         break;
+
+        if (isPlayer1Playing)
+        {
+           Console.WriteLine($"{}'s turn."); 
+        }
+
+        else
+        {
+            Console.WriteLine($"{}'s turn.");
+        }
      }
      Console.ReadKey();
    }
@@ -14,23 +24,29 @@ class UI {
 class Program {
 
    public static bool isPlaying = true;
-
+   bool isPlayer1Playing = true;
+   
+   enum turns {
+      X,O   
+   }
    public static string [,] cells =  { // string[][] can also be used
                                        {"1", "2", "3"},
                                        {"4", "5", "6"},
                                        {"7", "8", "9"},
                                     };
 
-      public static void showGrid() {
-         for (int i = 0; i < cells.GetLength(0); i++) {
-            for (int j = 0; j < cells.GetLength(1); j++) {
+    public static void showGrid() {
+      for (int i = 0; i < cells.GetLength(0); i++) {
+         for (int j = 0; j < cells.GetLength(1); j++) {
                Console.Write(' '+cells[i,j]);
                Console.Write("|");
             }
-            Console.WriteLine();
-            Console.WriteLine(" " + "~-~-~-~-");
+         Console.WriteLine();
+         Console.WriteLine(" " + "~-~-~-~-");
          }
       }
+
+    
 }
 
 // cells.GetLength(0); for first dimension of 1D
