b4d45fc4caa7562a0078f9869ba3b074ed89cbc7
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-09-04 19:03:38 +0545

    started to do the function
    ~ removed the previous CheckWin fn

--- Program.cs
+++ Program.cs
@@ -1,78 +1,69 @@
 ﻿using System;
 namespace Tic-Tac-Toe {
-   class UI {
-      static void Main(string [] args) {
-         Console.WriteLine("Tic-Tac-Toe game");
-         while (Program.isPlaying)
-         {
-            Program.ShowGrid(); 
-            break;
+	class UI {
+		static void Main(string [] args) {
+			Console.WriteLine("Tic-Tac-Toe game");
+			while (Program.isPlaying)
+			{
+				Program.ShowGrid(); 
 
-            if(Program.isPlayer1turn) {
-               Console.WriteLine("Player1's turn!");
-            } else {
-               Console.WriteLine("Player2's turn!");
-            }
-
-            Console.WriteLine("Choose what do you want to play as: \n\t1. X \n\t2. Y");
-            int chooseIndex = int.Parse(Console.ReadLine());
-
-            string choose = Program.turns[chooseIndex];
-            Program.isPlayer1turn =!Program.isPlayer1turn;
-
-            Program.count++;
-
-         }
-         Console.ReadKey();
-      }
-   }
-
-   class Program {
-
-      public static string[] turns = {"X" , "O"};
-      public static bool isPlaying = true;
-      public static bool isPlayer1turn = true;
-      public static int count = 0;
-
-      public static string [,] cells =  { // string[][] can also be used
-         {"1", "2", "3"},
-         {"4", "5", "6"},
-         {"7", "8", "9"},
-      };
-
-      public static void ShowGrid() {
-         for (int i = 0; i < cells.GetLength(0); i++) {
-            for (int j = 0; j < cells.GetLength(1); j++) {
-               Console.Write(' '+cells[i,j]);
-               Console.Write("|");
-            }
-            Console.WriteLine();
-            Console.WriteLine(" " + "~-~-~-~-");
-         }
-      }
-
-      public static void CheckIfWon() {
+				if(Program.isPlayer1turn) {
+					Console.WriteLine("Player1's turn!");
+				} else {
+					Console.WriteLine("Player2's turn!");
+				}
 
 
-         if ((cells[0,0] == cells[0,1] && cells[0,1] == cells[0,2]) || 
-               (cells[1,0] == cells[1,1] && cells[1,1] == cells[1,2]) || 
-               (cells[2,0] == cells[2,1] && cells[2,1] == cells[2,2]) || 
-               (cells[0,0] == cells[1,0] && cells[1,0] == cells[2,0]) || 
-               (cells[0,1] == cells[1,1] && cells[1,1] == cells[2,1]) || 
-               (cells[0,2] == cells[1,2] && cells[1,2] == cells[2,2]) || 
-               (cells[0,0] == cells[1,1] && cells[1,1] == cells[2,2]) || 
-               (cells[0,2] == cells[1,1] && cells[1,1] == cells[2,0])) {
+				Console.WriteLine("Choose what do you want to play as: \n\t1. X \n\t2. O");
+				int chooseIndex = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{} won.");
+				string choose = Program.turns[chooseIndex];
+				Program.isPlayer1turn =!Program.isPlayer1turn;
 
-         }
+				Program.count++;
 
-         else if (count == 9) {
-            Console.WriteLine("It is a draw!"); 
-         }
+				break;
+			}
+			Console.ReadKey();
+		}
+	}
 
-      }
-   }
+	class Program {
+
+		public static string[] turns = {"X" , "O"};
+		public static bool isPlaying = true;
+		public static bool isPlayer1turn = true;
+		public static int count = 0;
+
+		public static string [,] cells =  { // string[][] can also be used
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+		public static void ShowGrid() {
+			for ( int i = 0; i < cells.GetLength(0); i++) {
+				for ( int j = 0; j < cells.GetLength(1); j++) {
+					Console.Write(' '+cells[i,j]);
+					Console.Write("|");
+				}
+				Console.WriteLine();
+				Console.WriteLine(" " + "~-~-~-~-");
+			}
+		}
+
+		public static bool CheckIfWon(string player) {
+
+			for(int i = 0; i < 3; i++) {
+
+				if (cells[i, 0] == player && cells[i, 1] == player && cells[i, 2] == player) {
+					return true;
+				}
+
+			}
+
+		}
+	}
 }
 
 // cells.GetLength(0); for first dimension of 1D
@@ -83,4 +74,21 @@ namespace Tic-Tac-Toe {
 //C:\Users\Ganga Ram Phaijoo\OneDrive\Documents\BP\csharp\tictactoe\Program.cs(63,1): error CS1022: Type or namespace definition, or end-of-file expected [C:\Users\G
 //anga Ram Phaijoo\OneDrive\Documents\BP\csharp\tictactoe\tictactoe.csproj]
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+//if ((cells[0,0] == cells[0,1] && cells[0,1] == cells[0,2]) || 
+//		(cells[1,0] == cells[1,1] && cells[1,1] == cells[1,2]) || 
+//		(cells[2,0] == cells[2,1] && cells[2,1] == cells[2,2]) || 
+//		(cells[0,0] == cells[1,0] && cells[1,0] == cells[2,0]) || 
+//		(cells[0,1] == cells[1,1] && cells[1,1] == cells[2,1]) || 
+//		(cells[0,2] == cells[1,2] && cells[1,2] == cells[2,2]) || 
+//		(cells[0,0] == cells[1,1] && cells[1,1] == cells[2,2]) || 
+//		(cells[0,2] == cells[1,1] && cells[1,1] == cells[2,0])) {
+
+//	Console.WriteLine($"{} won.");
+
+//}
+
+//else if (count == 9) {
+//	Console.WriteLine("It is a draw!"); 
+//
